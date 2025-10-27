1777c734091f951d8a26943a2a09f05bfb76a85b
desmondcheongzx <desmondcheongzx@gmail.com>
2025-10-27 14:43:00 -0700

    modify workflow

--- my_workflow.py
+++ my_workflow.py
@@ -50,3 +50,22 @@ def my_workflow() -> dict:
 
     return {"results": results}
 
+
+def my_func(source: daft.DataFrame) -> dict:
+    count = 4
+    logger.info(f"Hello from my_workflow with count={count}")
+
+    source.show()
+
+    logger.info("My job is ready, starting queries..")
+
+    results = {}
+    for i in range(count):
+        logger.info(f"Starting query {i+1} of {count}.")
+        dest = mkdir()
+        source.write_parquet(dest)
+        results[f"run_{i + 1}"] = dest
+
+    logger.info("All queries are done, sending the results back.")
+
+    return {"results": results}
